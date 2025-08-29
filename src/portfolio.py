4950c6971261daae99bfbb67654e5b1cf8d48700
WhiteFalcon121 <58865937+WhiteFalcon121@users.noreply.github.com>
2025-08-29 16:01:11 +0100

    Implement trading logic for relative momentum

--- src/portfolio.py
+++ src/portfolio.py
@@ -1,9 +1,25 @@
 from collections import defaultdict
 
 class Portfolio():
+    '''
+    Simple portfolio class to manage cash and holdings.
+    Args:
+        initial_cash (int): Starting cash amount.
+    Attributes:
+        cash (int): Current cash amount.
+        holdings (defaultdict): Dictionary mapping ticker symbols to quantities held.
+    Methods:
+        tickers_list(): Returns a list of tickers currently in the portfolio.
+        sell(ticker, curr_price, quantity): Sells a specified quantity of a ticker at the current price.
+        buy(ticker, curr_price, quantity): Buys a specified quantity of a ticker at the current price.
+        __str__(): Returns a string representation of the portfolio's cash and holdings.
+    '''
     def __init__(self, initial_cash: int):
         self.cash = initial_cash
         self.holdings = defaultdict(int)  # ticker -> quantity
+
+    def tickers_list(self):
+        return list(self.holdings.keys()) # maybe more efficient to store list of tickers, add and remove as needed
     
     def sell(self, ticker: str, curr_price: float, quantity: int):
         if ticker in self.holdings and self.holdings[ticker] >= quantity:
--- src/signals.py
+++ src/signals.py
@@ -1,15 +1,22 @@
 import pandas as pd
+from src.portfolio import Portfolio
+from scripts.config import DAYS_12M, DAYS_3M, DAYS_1M
 
-# Trading day constants - move later to config
-DAYS_12M = 252 # Approx 252 trading days in a year
-DAYS_3M = 63  # Approx 63 trading days in 3 months
-DAYS_1M = 21  # Approx 21 trading days in a month
-
-def momentum_strategy(prices: pd.DataFrame):
+def momentum_strategy(prices: pd.DataFrame, portfolio: Portfolio, rebalance_dates: list):
+    '''
+    Implements relative momentum strategy.
+    
+    Args:
+        prices (pd.DataFrame): DataFrame with dates as index and tickers as columns, containing closing price data.
+        portfolio (Portfolio): Portfolio object to manage cash and holdings.
+        rebalance_dates (list): List of dates (as strings) on which to rebalance the portfolio.
+    Returns:
+        None: The function modifies the portfolio in place.
+    '''
     # precompute returns
     returns = [prices.pct_change(periods=DAYS_12M), prices.pct_change(periods=DAYS_3M), prices.pct_change(periods=DAYS_1M)]
 
-    rebalance_dates = (returns[0].index[DAYS_12M::DAYS_1M]) # rebalancing dates (every month after first 12 months)
+    # rebalance_dates = (returns[0].index[DAYS_12M::DAYS_1M]) # rebalancing dates (every month after first 12 months) ] now a parameter
 
     # for each date, rank stocks based on returns
     for date in rebalance_dates:
@@ -24,8 +31,30 @@ def momentum_strategy(prices: pd.DataFrame):
             latest_z_scores.append((latest_returns - latest_mean) / latest_std)
         momentum_scores = (0.5 * latest_z_scores[0]) + (0.3 * latest_z_scores[1]) + (0.2 * latest_z_scores[2])
 
-        ranked_stocks = momentum_scores.sort_values(ascending=False)[:10] # top 10 momentum stocks
+        ranked_stocks = (momentum_scores.sort_values(ascending=False)[:10]).index.tolist() # top 10 momentum stocks
 
-        # TODO: implement trading logic
+        # trading logic (adjust incrementally)
+        stocks_to_buy_num = 0
+
         # sell stocks not in ranked_stocks
+        for ticker in portfolio.tickers_list():
+            if ticker not in ranked_stocks:
+                # sell logic
+                curr_price = prices.loc[date, ticker]
+                quantity = portfolio.holdings[ticker]
+                portfolio.sell(ticker, curr_price, quantity)
+                stocks_to_buy_num += 1
+
+        cash_per_stock = portfolio.cash // stocks_to_buy_num if stocks_to_buy_num > 0 else 0 # equal weighting
+        
         # buy ranked_stocks
+        for ticker in ranked_stocks:
+            if ticker not in portfolio.holdings:
+                # buy logic
+                curr_price = prices.loc[date, ticker]
+                quantity = cash_per_stock // curr_price
+                if quantity > 0:
+                    portfolio.buy(ticker, curr_price, quantity)
+                    portfolio.holdings[ticker] += quantity
+                else:
+                    print(f"Not enough cash to buy any shares of {ticker} at {curr_price}")
